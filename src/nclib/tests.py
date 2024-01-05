981f364066ada67d723d8e18b48f788c2afd9cef
Damian Frąszczak <damian.fraszczak@wat.edu.pl>
2024-01-05 13:49:25 +0100

    code refactored

--- src/nclib/tests.py
+++ src/nclib/tests.py
@@ -1,8 +1,9 @@
 import networkx as nx
 
 from src.algorithms.diffusion_degree import diffusion_degree
+from src.algorithms.entropy_centrality import entropy_centrality
 from src.commons import print_dict
 
 g = nx.karate_club_graph()
 
-print_dict(diffusion_degree(g))
+print_dict(entropy_centrality(g))
