c556d43dc429fe62ead3383f3a830bcf3c7884fa
Edyta Frąszczak <efraszczak@motoinsight.com>
2024-01-06 12:27:34 +0100

    new algs

--- src/nclib/common/nx_cached.py
+++ src/nclib/common/nx_cached.py
@@ -1,8 +1,22 @@
+from functools import cache, lru_cache
 from typing import Any
 
 import networkx as nx
 from networkx import Graph
 
+MAX_SIZE = 64
 
-def cached_all_shortest_paths(network: Graph, source: Any, target: Any):
+
+@lru_cache(maxsize=MAX_SIZE)
+def all_shortest_paths(network: Graph, source: Any, target: Any):
     return nx.all_shortest_paths(network, source=source, target=target)
+
+
+@lru_cache(maxsize=MAX_SIZE)
+def single_source_shortest_path_length(network: Graph, v: Any):
+    return nx.single_source_shortest_path_length(network, v)
+
+
+@lru_cache(maxsize=MAX_SIZE)
+def all_pairs_shortest_path_length(network: Graph):
+    return nx.all_pairs_shortest_path_length(network)
--- src/nclib/tests.py
+++ src/nclib/tests.py
@@ -1,9 +1,9 @@
 import networkx as nx
 
-from nclib.algorithms.diffusion_degree import diffusion_degree
-from nclib.algorithms.entropy_centrality import entropy_centrality
+from nclib.algorithms.semi_local_centrality import semi_local_centrality
+from nclib.algorithms.topological_centrality import topological_centrality
 from nclib.commons import print_dict
 
 g = nx.karate_club_graph()
 
-print_dict(entropy_centrality(g))
+print_dict(topological_centrality(g))
