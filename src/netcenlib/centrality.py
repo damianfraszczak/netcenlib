7304c1f690478950f47f237ccf8269b20d9afd15
Damian Frąszczak <dfraszczak@motoinsight.com>
2024-02-22 11:05:53 +0100

    code cleanup

--- src/netcenlib/centrality.py
+++ src/netcenlib/centrality.py
@@ -1,11 +1,10 @@
 """Centrality measures for networks."""
 
 import netcenlib.algorithms as ncl_algorithms
-from netcenlib import Centrality
+from netcenlib.taxonomies import Centrality
 
 
 def compute_centrality(network, centrality: Centrality, *args, **kwargs):
     """Compute centrality measure for a given network."""
     centrality_function_name = f"{centrality.value.lower()}_centrality"
-    print(centrality_function_name)
     return getattr(ncl_algorithms, centrality_function_name)(network, *args, **kwargs)
